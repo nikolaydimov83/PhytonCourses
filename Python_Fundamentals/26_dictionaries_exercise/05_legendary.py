54ff28266ccf82cb2058967983565c1dce1fb422
Nikolay Dimov <nikolay.dimov83@gmail.com>
2024-01-07 19:02:04 +0200

    5th

--- Python_Fundamentals/26_dictionaries_exercise/05_legendary.py
+++ Python_Fundamentals/26_dictionaries_exercise/05_legendary.py
@@ -1,4 +1,8 @@
-dictionary={}
+dictionary={"shards":0, "fragments":0,"motes":0}
+specials={ 
+    "shards":"Shadowmourne",
+    "fragments":"Valanyr",
+    "motes":"Dragonwrath"}
 break_flag=False
 while True:   
     if break_flag==True:
@@ -6,15 +10,22 @@ while True:
     command_line=input().split(' ')
 
     for i in range(0,len(command_line), 2):
-        if command_line[i+1] not in dictionary:
-            dictionary[command_line[i+1]]=int(command_line[i])  
+        key = command_line[i+1].lower()
+        value=int(command_line[i])
+        if key not in dictionary:
+            dictionary[key.lower()]=int(value)  
         else:
-            dictionary[command_line[i+1]]+=int(command_line[command_line[i]]) 
-        if dictionary[command_line[i+1]]>=250:
-            print(f"{command_line[i+1]} obtained!")
+            dictionary[key.lower()]+=int(value) 
+        if dictionary[key]>=250:
+            dictionary[key]-=250
+            print(f"{specials[key]} obtained!")
             break_flag=True
             break
 
-print(f"shards: {dictionary.shards}")
-print(f"fragments: {dictionary.fragments}")
-print(f"motes: {dictionary.motes}")
+print(f"shards: {dictionary['shards']}")
+print(f"fragments: {dictionary['fragments']}")
+print(f"motes: {dictionary['motes']}")
+
+for item in dictionary.items():
+    if item[0] not in ['shards','fragments','motes']:
+        print(f"{item[0]}: {item[1]}")
