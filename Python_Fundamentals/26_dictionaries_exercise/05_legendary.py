b61c370756b457bf1e6919d9ba777366206f0ad8
Nikolay Dimov <nikolay.dimov83@gmail.com>
2024-01-07 19:04:13 +0200

    5 th ready

--- Python_Fundamentals/26_dictionaries_exercise/05_legendary.py
+++ Python_Fundamentals/26_dictionaries_exercise/05_legendary.py
@@ -16,7 +16,7 @@ while True:
             dictionary[key.lower()]=int(value)  
         else:
             dictionary[key.lower()]+=int(value) 
-        if dictionary[key]>=250:
+        if dictionary[key]>=250 and key in ['shards','fragments','motes']:
             dictionary[key]-=250
             print(f"{specials[key]} obtained!")
             break_flag=True
